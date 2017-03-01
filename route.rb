2302492a53c2a5bad65c09b9c38e37286cf4f9b2
Konstantin Mitko <kmitko@mail.ru>
2017-03-01 19:25:19 +0300

    Внесены изменения

--- route.rb
+++ route.rb
@@ -5,23 +5,21 @@
 
 # Переменные экземпляра класса Class
 @route_list = []   # длина маршрута
-
+@route_length = 0
 # Описание класса "Маршрут"
 def change_route
   # проверка статуса игры
   if @flag_game == true 
-    puts "----------------------------------------- \
-        \nYou can't change the length of the route, \
-        \nbecause traffic is already activated! \
-        \n-----------------------------------------"
+    puts "Message of game"
+    puts "You can't change the length of the route, \
+        \nbecause traffic is already activated!"
     return
   end
   # проверка наличия светофоров    
   if @ligth_list.size > 0
-    puts "----------------------------------------- \
-        \nYou can't change the length of the route, \
-        \nbecause traffic ligths were installed! \
-        \n-----------------------------------------"
+    puts "Message of game"
+    puts "You can't change the length of the route, \
+        \nbecause traffic ligths were installed!"
     return
   end
   @road.change_length
@@ -34,30 +32,28 @@ class Route
   attr_accessor :route_length
   # Показать длину маршрута ---------------------------------------------
   def show_length
-    puts "------------------------------------------- \
-        \nRoute length is #{@route_length} km \
-        \n-------------------------------------------"
+    puts "Info of game"
+    puts "Route length is #{@route_length} km"
   end
   # Изменить длину маршрута ---------------------------------------------
   def change_length    
-    print "----------------------------------------- \
-          \nRoute length is #{@route_length} km \
+    puts "Query of game"
+    print "Route length is #{@route_length} km \
           \nSet the new value of route length in km: "
     input = gets.chomp.to_i
     
     # проверка введенного значения     
     if input <= 0
-      puts "------------------------------------------------------------- \
-          \nInvalid value '#{input}' \
-          \nRoute length is unchanged and equal to #{@route_length} km \
-          \n-------------------------------------------------------------"
+      puts "Message of game"
+      puts "Invalid value '#{input}' \
+          \nRoute length is unchanged and equal to #{@route_length} km"
     else # Задание нового значение длины маршрута
       @route_length = input
-      puts "------------------------------------------- \
-          \nNew route length is #{@route_length} km \
-          \n-------------------------------------------"
+      puts "Info of game"
+      puts "New route length is #{@route_length} km"
     end    
   end
 end
 @road = Route.new
 @route_list.push(@road)
+@route_length = @road.route_length
