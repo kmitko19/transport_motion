c9457ea84296a71449b4017d827d4b3dacb2dc9d
Konstantin Mitko <kmitko@mail.ru>
2017-03-02 19:26:07 +0300

    Внесены изменения

--- route.rb
+++ route.rb
@@ -7,14 +7,14 @@
 def change_route
   # проверка статуса игры
   if @flag_game == true 
-    puts "Message of game"
+    puts "!"
     puts "You can't change the length of the route, \
         \nbecause traffic is already activated!"
     return
   end
   # проверка наличия светофоров    
   if @ligth_list.size > 0
-    puts "Message of game"
+    puts "!"
     puts "You can't change the length of the route, \
         \nbecause traffic ligths were installed!"
     return
@@ -30,24 +30,24 @@ class Route
   attr_accessor :route_length
   # Показать длину маршрута ---------------------------------------------
   def show_length
-    puts "Info of game"
+    puts "*"
     puts "Route length is #{@route_length} km"
   end
   # Изменить длину маршрута ---------------------------------------------
   def change_length    
-    puts "Query of game"
+    puts ">"
     print "Route length is #{@route_length} km \
           \nSet the new value of route length in km: "
     input = gets.chomp.to_i
     
     # проверка введенного значения     
     if input <= 0
-      puts "Message of game"
+      puts "!"
       puts "Invalid value '#{input}' \
           \nRoute length is unchanged and equal to #{@route_length} km"
     else # Задание нового значение длины маршрута
       @route_length = input
-      puts "Info of game"
+      puts "*"
       puts "New route length is #{@route_length} km"
     end    
   end
